6bbab2ab8d5f36845f645d82bd39e5564e24a1d7
Shavz <104979137+Shavz@users.noreply.github.com>
2022-10-29 13:19:56 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -3,7 +3,9 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Review class that inherits from BaseModel"""
+    """
+    Review class that inherits from BaseModel
+    """
 
     place_id = ""
     user_id = ""
