7dd7602223fe29ba7d425dccddd4bf3088ac1a93
Shavz <104979137+Shavz@users.noreply.github.com>
2022-10-29 14:28:41 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-    Defines a class FileStorage.
+Defines a class FileStorage.
 """
 
 import json
@@ -16,30 +16,40 @@ from models.review import Review
 
 
 class FileStorage:
-    """Represent a FileStorage"""
+    """
+    Represent FileStorage
+    """
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Return all the objects saved in the file"""
+        """
+        Return all the objects saved in the file
+        """
 
         return self.__objects
 
     def new(self, obj):
-        """Add new objects in a dictionary"""
+        """
+        Add new objects in a dictionary
+        """
         objId = obj.__class__.__name__ + '.' + obj.id
         self.__objects[objId] = obj
 
     def save(self):
-        """Save object representation of JSON to a file"""
+        """
+        Save object representation of json to a file
+        """
 
         with open(self.__file_path, mode='w', encoding='UTF-8') as myfile:
             to_json = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(to_json, myfile)
 
     def reload(self):
-        """Function that creates an Object from a JSON file"""
+        """
+        Function that creates an Object from a json file
+        """
 
         from_json = {}
         try:
