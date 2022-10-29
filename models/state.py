1d17109de0328fbd7fb66a83164e2c5975d282cd
Shavz <104979137+Shavz@users.noreply.github.com>
2022-10-29 13:20:10 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -3,6 +3,8 @@ from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """State class that inherits from BaseModel"""
+    """
+    State class that inherits from BaseModel
+    """
 
     name = ""
