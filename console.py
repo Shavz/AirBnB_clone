d3c223efa9fc9f7c4641f4071415b07f25b16b78
Shavz <104979137+Shavz@users.noreply.github.com>
2022-10-29 14:27:17 +0200

    Update console.py

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-"""Defines the HBNH command line."""
-
+"""
+Entry point of the command interpreter
+"""
 
 import cmd
 import json
@@ -19,7 +19,9 @@ from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    """Hbnb command processor."""
+    """
+    Hbnb class definition
+    """
 
     prompt = '(hbnb) '
     file = None
@@ -42,7 +44,9 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def precmd(self, line):
-        """ Get the line before interpretate"""
+        """
+        Get the line before interpretation
+        """
         if len(line):
             l_c = line.split()
             if len(l_c):
@@ -88,7 +92,10 @@ class HBNBCommand(cmd.Cmd):
             return line
 
     def do_count(self, argv):
-        """Count how much instances have a given class"""
+        """
+        Count the amount of instances in a given class
+        """
+        
         l_c = argv.split()
         all_instances = storage.all()
         if l_c[0] in self.__classes:
@@ -101,36 +108,50 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
 
     def help_count(self):
-        """Help command for count"""
+        """
+        Help command for count
+        """
 
-        msg = "Count how much instances have a given class\n"
+        msg = "Count the amount of instances in a given class\n"
         print(msg)
 
     def emptyline(self):
-        """Empty line method"""
+        """
+        Empty line
+        """
         pass
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """
+        Quit command to exit the program
+        """
 
         quit()
         return True
 
     def help_quit(self):
-        """Help command for quit"""
+        """
+        Help command for quitting the program
+        """
         print("Quit command to exit the program\n")
 
     def do_EOF(self, line):
-        """EOF command to exit the program"""
+        """
+        EOF command to exit the program
+        """
         print()
         return True
 
     def help_EOF(self):
-        """Help command for EOF"""
+        """
+        Help command for EOF
+        """
         print("EOF command to exit the program\n")
 
     def do_create(self, arg):
-        """Create a BaseModel and save the json in a file"""
+        """
+        Create a BaseModel and save json in a file
+        """
 
         if len(arg) > 0:
             list_arg = arg.split()
@@ -143,12 +164,16 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def help_create(self):
-        """Help command for create"""
-        print("Create a BaseModel and save the json in a file\n")
+        """
+        Help command for create
+        """
+        print("Create a BaseModel and save json in a file\n")
 
     def do_show(self, arg):
-        """Prints the string representation of an instance
-        based on the class name and id"""
+        """
+        Print the string representation of an instance
+        based on the class name and id
+        """
 
         if len(arg) > 0:
             list_arg = arg.split()
@@ -172,14 +197,18 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def help_show(self):
-        """Help command for show"""
+        """
+        Help command
+        """
 
         msg = "Prints the string representation of an instance "
         msg += "based on the class name and id\n"
         print(msg)
 
     def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id"""
+        """
+        Deletes an instance based on the class name and id
+        """
 
         if len(arg) > 0:
             list_arg = arg.split()
@@ -204,12 +233,16 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def help_destroy(self):
-        """Help command for destroy"""
+        """
+        Help command to destroy
+        """
         print("Deletes an instance based on the class name and id\n")
 
     def do_all(self, arg):
-        """Prints all string representation of all instances
-        based or not on the class name"""
+        """
+        Prints all string representation of all instances
+        based on the class name
+        """
 
         objects = storage.all()
         if len(arg) > 0:
@@ -225,15 +258,19 @@ class HBNBCommand(cmd.Cmd):
             print(allObjs)
 
     def help_all(self):
-        """Help command for all"""
+        """
+        Help command
+        """
 
         msg = "Prints all string representation of all instances "
         msg += "based or not on the class name\n"
         print(msg)
 
     def do_update(self, arg):
-        """Updates an instance based on the class name
-        and id by adding or updating attribute"""
+        """
+        Updates an instance based on the class name
+        and id by adding or updating attribute
+        """
 
         if len(arg) > 0:
             objects = storage.all()
@@ -284,7 +321,9 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def help_update(self):
-        """Help command for update"""
+        """
+        Help command for updating
+        """
 
         msg = "Updates an instance based on the class "
         msg += "name and id by adding or updating attribute\n"
