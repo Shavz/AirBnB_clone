64760bd5f879ecebc9d681deae8151e011411d4d
Shavz <104979137+Shavz@users.noreply.github.com>
2022-10-29 14:30:47 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -8,7 +8,7 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Represent a City."""
+    """City class that inherits from BaseModel"""
 
     state_id = ""
     name = ""
